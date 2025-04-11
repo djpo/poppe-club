53fc74e96b6314703c22178475841e26e8355a50
Poppe <djpo@users.noreply.github.com>
2025-04-11 11:32:49 +0200

    rm template styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,20 +39,4 @@
 		max-width: 1440px;
 		padding: 1em;
 	}
-
-	/* footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-	footer a {
-		font-weight: bold;
-	}
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	} */
 </style>
