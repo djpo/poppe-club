bcc47a8e0103079fd84c3badb4708b8ed43643b0
Poppe <djpo@users.noreply.github.com>
2025-04-13 22:11:14 +0200

    add placeholder: rainbow cancan w Counter (migrated to svelte5)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+  import "../global.css";
+
+  let { children } = $props();
+</script>
+
+<div class="app">
+  <main>
+    {@render children?.()}
+  </main>
+</div>
+
+<style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    min-width: 100vw;
+  }
+
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,57 @@
-<h1>Welcome to your library project</h1>
-<p>
-  Create your package using @sveltejs/package and preview/showcase your work
-  with SvelteKit
-</p>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<script>
+  import cancan from "$lib/images/cancan.webp";
+  import Counter from "./Counter.svelte";
+</script>
+
+<svelte:head>
+  <title>poppe.club</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<img class="background-video" src={cancan} alt="welcome!!!!!!" />
+
+<section>
+  <h1 class="gray-box">welcome to poppe.club</h1>
+
+  <h2 class="gray-box">fresh site incoming</h2>
+
+  <div class="counter-container gray-box">
+    <p class="counter-label">
+      while you wait,<br />here,<br />count some electric sheep:
+    </p>
+    <Counter />
+  </div>
+</section>
+
+<style>
+  .background-video {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    z-index: -1;
+    opacity: 0.4;
+  }
+
+  section {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .gray-box {
+    border: 2px solid var(--gray-box-border);
+    background-color: var(--gray-box-background);
+    padding: 1rem 1.2rem;
+  }
+
+  .counter-container {
+    margin-top: 6rem;
+  }
+
+  .counter-label {
+    margin: 0 0 1rem 0;
+  }
+</style>
