6d1ca327b528180075cffd47e44b66e6466a93cd
Poppe <djpo@users.noreply.github.com>
2025-04-27 20:36:51 +0200

    extract Footer to separate component

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,66 @@
+<div class="footer-chunks-container">
+  <div class="footer-chunk-wider">
+    <h2>Poppe</h2>
+    <p>
+      Software developer
+      <br />
+      Visit me on
+      <a href="https://linkedin.com/in/poppepoppe" target="_blank">LinkedIn</a>
+      & <a href="https://github.com/djpo" target="_blank">GitHub</a>
+      <br />
+      Download my
+      <a
+        href="https://drive.google.com/file/d/1tAV-kmS_XbPHGOBskXjUu8u0kB6Sxbfd/view"
+        target="_blank">CV</a
+      >
+      <br />
+      Check out my music on
+      <a href="https://soundcloud.com/poppe-poppe" target="_blank">Soundcloud</a
+      >
+    </p>
+  </div>
+
+  <div>
+    <h2>Contact</h2>
+    <p>
+      poppeisadancer@gmail.com
+      <br />
+      +49 176 7126346
+    </p>
+  </div>
+
+  <div>
+    <h2>More</h2>
+    <p>
+      <a href="/about">About me</a>
+      <br />
+      <a href="/privacy">Privacy policy</a>
+      <br />
+      Design by
+      <a href="https://janagramlich.com/" target="_blank">Jana Gramlich</a>
+    </p>
+  </div>
+</div>
+
+<style>
+  .footer-chunks-container {
+    margin-top: 2em;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1em;
+  }
+  @media (max-width: 719px) {
+    .footer-chunks-container {
+      margin-top: 1em;
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+  .footer-chunk-wider {
+    min-width: 280px;
+  }
+  @media (max-width: 719px) {
+    .footer-chunk-wider {
+      grid-column: 1 / span 2;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import Nav from "$lib/Nav.svelte";
+  import Footer from "$lib/Footer.svelte";
   import BubbleList from "$lib/BubbleList.svelte";
   import ProjectImage from "$lib/ProjectImage.svelte";
   import { homeContent } from "./content.ts";
@@ -38,51 +39,7 @@
     {/each}
   </div>
 
-  <div class="footer-chunks-container">
-    <div class="footer-chunk-wider">
-      <h2>Poppe</h2>
-      <p>
-        Software developer
-        <br />
-        Visit me on
-        <a href="https://linkedin.com/in/poppepoppe" target="_blank">LinkedIn</a
-        >
-        & <a href="https://github.com/djpo" target="_blank">GitHub</a>
-        <br />
-        Download my
-        <a
-          href="https://drive.google.com/file/d/1tAV-kmS_XbPHGOBskXjUu8u0kB6Sxbfd/view"
-          target="_blank">CV</a
-        >
-        <br />
-        Check out my music on
-        <a href="https://soundcloud.com/poppe-poppe" target="_blank"
-          >Soundcloud</a
-        >
-      </p>
-    </div>
-
-    <div>
-      <h2>Contact</h2>
-      <p>
-        poppeisadancer@gmail.com
-        <br />
-        +49 176 7126346
-      </p>
-    </div>
-
-    <div>
-      <h2>More</h2>
-      <p>
-        <a href="/about">About me</a>
-        <br />
-        <a href="/privacy">Privacy policy</a>
-        <br />
-        Design by
-        <a href="https://janagramlich.com/" target="_blank">Jana Gramlich</a>
-      </p>
-    </div>
-  </div>
+  <Footer />
 </section>
 
 <style>
@@ -91,25 +48,6 @@
       display: grid;
       grid-template-columns: repeat(auto-fit, minmax(21em, 1fr));
       gap: 1em;
-    }
-  }
-
-  .footer-chunks-container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 1em;
-  }
-  @media (max-width: 719px) {
-    .footer-chunks-container {
-      grid-template-columns: repeat(2, 1fr);
-    }
-  }
-  .footer-chunk-wider {
-    min-width: 280px;
-  }
-  @media (max-width: 719px) {
-    .footer-chunk-wider {
-      grid-column: 1 / span 2;
     }
   }
   .projects {
